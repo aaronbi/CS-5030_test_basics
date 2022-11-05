0fe7e301fb77427f47e9654732aa443e852734cc
aaronbi <99830616+aaronbi@users.noreply.github.com>
2022-11-05 18:53:10 -0500

    writing test cases

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -18,6 +18,32 @@ describe('todo test suite', () => {
     test("get_todos", () => {
         expect(todo_service.get_todos().todo.length).toEqual(3);
     });
+    
+    
+    test("add_todos", () => {
+        let todo_service = new todoservice();
+        todo_service.add_todo("test element");
+        
+        expect(todo_service.get_todos().todo.length).toEqual(4);
+    });
+    
+    test("delete_todos", () => {
+        let todo_service = new todoservice();
+        let out = todo_service.remove_todo(0);
+        expect(todo_service.get_todos().todo.length).toEqual(2);
+        expect(out).toBe(true);
+    });
+    
+    
+    test("update_todos", () => {
+        let todo_service = new todoservice();
+        todo_service.add_todo("test element");
+        
+        expect(todo_service.get_todos().todo[3]).toEqual("test element");
+        todo_service.update_todo(3, "new");
+        
+        expect(todo_service.get_todos().todo[3]).toEqual("new");
+    });
 
     
 
